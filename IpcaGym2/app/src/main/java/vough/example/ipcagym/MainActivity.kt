334000120e1b98b1204d7b879201692254a17293
Presentation12 <joaocarlosapresentacao@gmail.com>
2022-11-07 15:29:59 +0000

    Inicio do layoput
    Login + atalho da app

---
+++ IpcaGym2/app/src/main/java/vough/example/ipcagym/MainActivity.kt
@@ -0,0 +1,11 @@
+package vough.example.ipcagym
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
