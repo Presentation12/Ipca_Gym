1aef6c30c67c43fd4826988e58b527fa29002c21
Pedro Simões <69718437+L0ud3r@users.noreply.github.com>
2022-12-23 00:25:08 +0000

    [15] Login Funcionario

---
+++ Backend_IPCA_Gym/LayerBOL/Models/LoginFuncionario.cs
@@ -0,0 +1,16 @@
+﻿namespace LayerBOL.Models
+{
+    public class LoginFuncionario
+    {
+        /// <summary>
+        /// Codigo de identificacao do funcionario
+        /// </summary>
+        /// <example>12543587</example>
+        public int codigo { get; set; }
+        /// <summary>
+        /// Password do funcionario
+        /// </summary>
+        /// <example>1234abcd</example>
+        public string password { get; set; }
+    }
+}
