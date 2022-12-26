9b90528924a9401a3b118a25026d4fb9f16f3fb6
Pedro Simões <69718437+L0ud3r@users.noreply.github.com>
2022-12-26 17:39:31 +0000

    Backend nada avançado

--- IpcaGym2/app/src/main/java/vough/example/ipcagym/MainActivity.kt
+++ IpcaGym2/app/src/main/java/vough/example/ipcagym/MainActivity.kt
@@ -2,10 +2,11 @@ package vough.example.ipcagym
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login_funcionario)
     }
 }
