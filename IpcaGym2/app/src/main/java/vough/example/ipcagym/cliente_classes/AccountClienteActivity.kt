0a3a49a491cf62d2a0b1051d3abdbc444e51ecf4
Presentation12 <joaocarlosapresentacao@gmail.com>
2022-12-27 17:44:47 +0000

    Frontend cliente account

--- IpcaGym2/app/src/main/java/vough/example/ipcagym/cliente_classes/AccountClienteActivity.kt
+++ IpcaGym2/app/src/main/java/vough/example/ipcagym/cliente_classes/AccountClienteActivity.kt
@@ -1,4 +1,12 @@
 package vough.example.ipcagym.cliente_classes
 
-class AccountClienteActivity {
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import vough.example.ipcagym.R
+
+class AccountClienteActivity : AppCompatActivity(){
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_cliente_account)
+    }
 }
