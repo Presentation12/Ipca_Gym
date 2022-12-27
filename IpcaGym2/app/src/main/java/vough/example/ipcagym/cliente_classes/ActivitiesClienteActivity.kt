9b9439fdf9b354531b0a6d969f18ac770451897d
Pedro Simões <69718437+L0ud3r@users.noreply.github.com>
2022-12-27 18:36:44 +0000

    Update ActivityList

--- IpcaGym2/app/src/main/java/vough/example/ipcagym/cliente_classes/ActivitiesClienteActivity.kt
+++ IpcaGym2/app/src/main/java/vough/example/ipcagym/cliente_classes/ActivitiesClienteActivity.kt
@@ -35,7 +35,7 @@ class ActivitiesClienteActivity : AppCompatActivity(){
         activityList.add(Atividade(3,2,4,
             LocalDateTime.of(2022,10,10,10,10,10),
             LocalDateTime.of(2022,10,10,11,10,10)))
-        /*activityList.add(Atividade(4,2,4,
+        activityList.add(Atividade(4,2,4,
             LocalDateTime.of(2022,12,13,10,10,10),
             LocalDateTime.of(2022,11,16,14,12,50)))
         activityList.add(Atividade(5,2,4,
@@ -79,7 +79,7 @@ class ActivitiesClienteActivity : AppCompatActivity(){
             LocalDateTime.of(2022,11,16,14,12,50)))
         activityList.add(Atividade(0,2,4,
             LocalDateTime.of(2022,12,13,10,10,10),
-            LocalDateTime.of(2022,11,16,14,12,50)))*/
+            LocalDateTime.of(2022,11,16,14,12,50)))
 
         val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_navbar)
 
