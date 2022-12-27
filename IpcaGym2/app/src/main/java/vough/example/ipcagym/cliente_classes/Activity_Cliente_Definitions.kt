0af2ddf82e27b616e3df4de9819daf0168121d8b
Goncalo Cunha <goncalo32661@gmail.com>
2022-12-27 20:48:13 +0000

    Activity_cliente_definitions

---
+++ IpcaGym2/app/src/main/java/vough/example/ipcagym/cliente_classes/Activity_Cliente_Definitions.kt
@@ -0,0 +1,15 @@
+package vough.example.ipcagym.cliente_classes
+
+import android.os.Bundle
+import android.view.View
+import android.widget.*
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import vough.example.ipcagym.R
+
+class Activity_Cliente_Definitions : AppCompatActivity(){
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_definitions_cliente)
+    }
+}
