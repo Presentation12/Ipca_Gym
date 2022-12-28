a30978700f11531290403f2d608bd8978f43b296
Presentation12 <joaocarlosapresentacao@gmail.com>
2022-12-28 15:42:08 +0000

    Frontend marcar consulta + Fix

--- IpcaGym2/app/src/main/java/vough/example/ipcagym/cliente_classes/MarcacaoClienteActivity.kt
+++ IpcaGym2/app/src/main/java/vough/example/ipcagym/cliente_classes/MarcacaoClienteActivity.kt
@@ -1,4 +1,14 @@
 package vough.example.ipcagym.cliente_classes
 
-class MarcacaoClienteActivity {
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import vough.example.ipcagym.R
+
+class MarcacaoClienteActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_cliente_marcacao_consulta)
+    }
+
 }
