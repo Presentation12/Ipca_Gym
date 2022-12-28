1c1bb1677cfc2d75ff9728093c4c6a093ec89f85
Pedro Simões <69718437+L0ud3r@users.noreply.github.com>
2022-12-28 20:09:46 +0000

    FuncionarioCapacidade (scroll fixo por fazer e barras por fazer)

--- IpcaGym2/app/src/main/java/vough/example/ipcagym/funcionarios_classes/CapacityManagementActivity.kt
+++ IpcaGym2/app/src/main/java/vough/example/ipcagym/funcionarios_classes/CapacityManagementActivity.kt
@@ -1,4 +1,40 @@
 package vough.example.ipcagym.funcionarios_classes
 
-class CapacityManagementActivity {
+import android.os.Bundle
+import android.view.View
+import android.widget.*
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.bottomappbar.BottomAppBar
+import vough.example.ipcagym.R
+
+class CapacityManagementActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_funcionario_capacity)
+
+        //findViewById<BottomAppBar>(R.id.bottom_navbar).setSelectedItemId(R.id.nav_capacity)
+
+        val imageView = findViewById<ImageView>(R.id.profile_pic_activity)
+        val spinner = findViewById<Spinner>(R.id.spinner)
+
+        val options = arrayOf("Conta", "Definições", "Sair")
+
+        val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, options)
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        spinner.adapter = adapter
+
+        spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(parent: AdapterView<*>, view: View, position: Int, id: Long) {
+                Toast.makeText(this@CapacityManagementActivity,options[position], Toast.LENGTH_LONG).show()
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>) {
+                // Do nothing
+            }
+        }
+
+        imageView.setOnClickListener {
+            spinner.performClick()
+        }
+    }
 }
