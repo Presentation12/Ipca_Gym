d230bcc8b1e7bce8371c1f1a4f05e82353bdd8b2
Goncalo Cunha <goncalo32661@gmail.com>
2022-12-28 23:58:49 +0000

    Criação de atividades .kt nas respetivas pastas

---
+++ IpcaGym2/app/src/main/java/vough/example/ipcagym/cliente_classes/PaginaInicialClienteActivity.kt
@@ -0,0 +1,12 @@
+package vough.example.ipcagym.cliente_classes
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import vough.example.ipcagym.R
+
+class PaginaInicialClienteActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_cliente_pagina_inicial)
+    }
+}
---
+++ IpcaGym2/app/src/main/java/vough/example/ipcagym/funcionarios_classes/PaginaInicialFuncionarioActivity.kt
@@ -0,0 +1,15 @@
+package vough.example.ipcagym.funcionarios_classes
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.isVisible
+import vough.example.ipcagym.R
+
+class PaginaInicialFuncionarioActivity: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_funcionario_pagina_inicial)
+    }
+}
