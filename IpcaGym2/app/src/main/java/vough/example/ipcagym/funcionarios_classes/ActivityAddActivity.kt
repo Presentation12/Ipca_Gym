eaeffd2bb89a7d0c01b59740c9bc73df5f289198
Pedro Simões <69718437+L0ud3r@users.noreply.github.com>
2022-12-28 12:13:09 +0000

    Add atividade e Details Atividade pelo Funcionario (Add não funciona corretamente)

--- IpcaGym2/app/src/main/java/vough/example/ipcagym/funcionarios_classes/ActivityAddActivity.kt
+++ IpcaGym2/app/src/main/java/vough/example/ipcagym/funcionarios_classes/ActivityAddActivity.kt
@@ -1,4 +1,88 @@
 package vough.example.ipcagym.funcionarios_classes
 
-class ActivityAddActivity {
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import android.view.ViewGroup
+import android.widget.*
+import androidx.appcompat.app.AppCompatActivity
+import vough.example.ipcagym.R
+import java.time.LocalDateTime
+
+class ActivityAddActivity : AppCompatActivity() {
+    var client_adapter = ActivityAddAdapter()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_activity_funcionario)
+
+        //Botao de concluir criação
+        val buttonAdd = findViewById<Button>(R.id.buttonAddActivity)
+        val buttonEntry = findViewById<Button>(R.id.entryButton)
+        val buttonExit = findViewById<Button>(R.id.exitButton)
+        val id_ginasio = findViewById<EditText>(R.id.newIDGymActivity)
+        val id_cliente = findViewById<EditText>(R.id.newIDClienteActivity)
+        var state = true
+
+        buttonEntry.setOnClickListener{
+            state = true
+            Toast.makeText(this@ActivityAddActivity,"Activity marked as entry!", Toast.LENGTH_SHORT).show()
+        }
+
+        buttonExit.setOnClickListener{
+            state = false
+            Toast.makeText(this@ActivityAddActivity,"Activity marked as exit!", Toast.LENGTH_SHORT).show()
+        }
+
+        buttonAdd.setOnClickListener{
+            val intent = Intent()
+
+            intent.putExtra("id_ginasio", id_ginasio.text.toString().toInt())
+            intent.putExtra("id_cliente", id_cliente.text.toString().toInt())
+            intent.putExtra("state", state)
+
+            setResult(RESULT_OK, intent)
+            finish()
+        }
+
+        val spinner = findViewById<Spinner>(R.id.spinner)
+        val options = arrayOf("Conta", "Definições", "Sair")
+        val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, options)
+        val imageView = findViewById<ImageView>(R.id.profile_pic_activity)
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        spinner.adapter = adapter
+
+        spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(parent: AdapterView<*>, view: View, position: Int, id: Long) {
+                Toast.makeText(this@ActivityAddActivity,options[position], Toast.LENGTH_LONG).show()
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>) {
+                // Do nothing
+            }
+        }
+
+        imageView.setOnClickListener {
+            spinner.performClick()
+        }
+    }
+
+    inner class ActivityAddAdapter : BaseAdapter(){
+        override fun getCount(): Int {
+            TODO("Not yet implemented")
+        }
+
+        override fun getItem(position: Int): Any {
+            TODO("Not yet implemented")
+        }
+
+        override fun getItemId(position: Int): Long {
+            TODO("Not yet implemented")
+        }
+
+        override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
+            TODO("Not yet implemented")
+        }
+
+    }
 }
--- IpcaGym2/app/src/main/java/vough/example/ipcagym/funcionarios_classes/FuncionarioManagementDetailsActivity.kt
+++ IpcaGym2/app/src/main/java/vough/example/ipcagym/funcionarios_classes/FuncionarioManagementDetailsActivity.kt
@@ -1,4 +1,67 @@
 package vough.example.ipcagym.funcionarios_classes
 
-class FuncionarioManagementDetailsActivity {
+import android.graphics.Color
+import android.os.Bundle
+import android.view.View
+import android.widget.*
+import androidx.appcompat.app.AppCompatActivity
+import org.w3c.dom.Text
+import vough.example.ipcagym.R
+
+class FuncionarioManagementDetailsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_activity_details_funcionario)
+
+        //fazer na outra activity
+        val id_atividade = intent.getIntExtra("id_atividade", -1)
+        val id_ginasio = intent.getIntExtra("id_atividade", -1)
+        val id_cliente = intent.getIntExtra("id_cliente", -1)
+        val data = intent.getStringExtra("data")
+        val data_E = intent.getStringExtra("data_E")
+        val hora_entrada = intent.getStringExtra("hora_entrada")
+        val hora_saida = intent.getStringExtra("hora_saida")
+        val state = intent.getBooleanExtra("state", true)
+
+        //Escrever no layout detalhado
+        findViewById<TextView>(R.id.idAtividadeAtividadeDetailFuncionario).text = id_atividade.toString()
+        findViewById<TextView>(R.id.idGinasioAtividadeDetailFuncionario).text = id_ginasio.toString()
+        findViewById<TextView>(R.id.idClienteAtividadeDetailFuncionario).text = id_cliente.toString()
+        val dataView = findViewById<TextView>(R.id.dataAtividadeDetailFuncionario)
+        val hora = findViewById<TextView>(R.id.horaAtividadeDetailFuncionario)
+
+        if(state == true){
+            hora.text = hora_entrada
+            hora.setTextColor(Color.GREEN)
+            findViewById<TextView>(R.id.textViewHourDetailFuncionario).text = "  Entry Hour"
+            dataView.text = data
+        }
+        else{
+            hora.text = hora_saida
+            hora.setTextColor(Color.RED)
+            findViewById<TextView>(R.id.textViewHourDetailFuncionario).text = "  Exit Hour"
+            dataView.text = data_E
+        }
+
+        val spinner = findViewById<Spinner>(R.id.spinner)
+        val options = arrayOf("Conta", "Definições", "Sair")
+        val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, options)
+        val imageView = findViewById<ImageView>(R.id.profile_pic_activity)
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        spinner.adapter = adapter
+
+        spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(parent: AdapterView<*>, view: View, position: Int, id: Long) {
+                Toast.makeText(this@FuncionarioManagementDetailsActivity,options[position], Toast.LENGTH_LONG).show()
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>) {
+                // Do nothing
+            }
+        }
+
+        imageView.setOnClickListener {
+            spinner.performClick()
+        }
+    }
 }
