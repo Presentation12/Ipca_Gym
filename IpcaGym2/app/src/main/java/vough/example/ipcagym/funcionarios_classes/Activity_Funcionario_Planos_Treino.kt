d1fd2b893fbb18a3d9627c1894106f850fd3429e
Pedro Simões <69718437+L0ud3r@users.noreply.github.com>
2022-12-31 12:54:49 +0000

    Add new plan completo com leitura de imagens

--- IpcaGym2/app/src/main/java/vough/example/ipcagym/funcionarios_classes/Activity_Funcionario_Planos_Treino.kt
+++ IpcaGym2/app/src/main/java/vough/example/ipcagym/funcionarios_classes/Activity_Funcionario_Planos_Treino.kt
@@ -1,7 +1,10 @@
 package vough.example.ipcagym.funcionarios_classes
 
+import android.Manifest
 import android.app.Activity
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
 import android.view.View
 import android.view.ViewGroup
@@ -9,10 +12,12 @@ import android.widget.*
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.net.toUri
 import vough.example.ipcagym.R
 import vough.example.ipcagym.cliente_classes.PlanoTreinoExerciciosClienteActivity
 import vough.example.ipcagym.data_classes.Exercicio
 import vough.example.ipcagym.data_classes.Plano_Treino
+import java.net.URI
 import java.time.LocalTime
 
 class Activity_Funcionario_Planos_Treino : AppCompatActivity() {
@@ -45,11 +50,17 @@ class Activity_Funcionario_Planos_Treino : AppCompatActivity() {
         list_view_planos_treino.adapter = plano_adapter
 
         receiverNewData = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
-
             if(it.resultCode == Activity.RESULT_OK){
-                //Buscar variaveis no intent
+                val id_plano_treino = it.data?.getIntExtra("id_plano_treino", 0)
+                val id_ginasio = it.data?.getIntExtra("id_ginasio", 0)
+                val tipo = it.data?.getStringExtra("tipo")
+                var foto_plano_treino = it.data?.getStringExtra("foto_plano_treino")
 
-                //add na lista
+                if(foto_plano_treino == "" || foto_plano_treino == null){
+                    foto_plano_treino = null
+                }
+
+                planos_treino_list.add(Plano_Treino(id_plano_treino, id_ginasio, tipo, foto_plano_treino))
                 plano_adapter.notifyDataSetChanged()
             }
         }
@@ -88,12 +99,18 @@ class Activity_Funcionario_Planos_Treino : AppCompatActivity() {
 
         override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
             val rootView = layoutInflater.inflate(R.layout.row_plano_treino, parent, false)
+            val permissions = arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE)
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                requestPermissions(permissions, 100)
+            }
 
             //Guardar elementos em variaveis
             val plano_treino_view = rootView.findViewById<TextView>(R.id.textViewPlanoTreino)
+            val plano_treino_image = rootView.findViewById<ImageView>(R.id.imageViewPlanoTreino)
 
             //Adicionar os textos
             plano_treino_view.text = planos_treino_list[position].tipo
+            plano_treino_image.setImageURI(planos_treino_list[position].foto_plano_treino?.toUri())
 
             //Clicar num rootView abre o plano de treino
             rootView.setOnClickListener {
@@ -109,6 +126,21 @@ class Activity_Funcionario_Planos_Treino : AppCompatActivity() {
 
             return rootView
         }
+    }
 
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when (requestCode) {
+            100 -> {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // Permission was granted. Do the read operation.
+                } else {
+                    // Permission denied. Show a message to the user.
+                }
+                return
+            }
+            // Handle other request codes.
+        }
     }
 }
--- IpcaGym2/app/src/main/java/vough/example/ipcagym/funcionarios_classes/Activity_Funcionario_Planos_Treino_Add.kt
+++ IpcaGym2/app/src/main/java/vough/example/ipcagym/funcionarios_classes/Activity_Funcionario_Planos_Treino_Add.kt
@@ -2,26 +2,68 @@ package vough.example.ipcagym.funcionarios_classes
 
 import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
-import android.widget.TextView
+import android.view.View
+import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
 import vough.example.ipcagym.R
 
 class Activity_Funcionario_Planos_Treino_Add : AppCompatActivity() {
+    var newImageValue = ""
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_funcionario_planos_treino_add)
+
+        val image_view = findViewById<ImageView>(R.id.profile_pic)
+        val spinner = findViewById<Spinner>(R.id.spinner)
+        val options = arrayOf("Conta", "Definições", "Sair")
+        val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, options)
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        spinner.adapter = adapter
+
+        findViewById<Button>(R.id.addPhotoPlanNew).setOnClickListener{
+            val intent = Intent(Intent.ACTION_PICK)
+            intent.type = "image/*"
+            //100 é o request code de escolher imagem
+            startActivityForResult(intent, 100)
+        }
 
         findViewById<Button>(R.id.addNewPlanButton).setOnClickListener{
             val intent = Intent()
 
             intent.putExtra("id_plano_treino", 1)
             intent.putExtra("id_ginasio", 1)
-            intent.putExtra("tipo", findViewById<TextView>(R.id.typePlanoValue).text)
-            intent.putExtra("foto_plano_treino", 1)
-
+            intent.putExtra("foto_plano_treino", newImageValue)
+            intent.putExtra("tipo", findViewById<EditText>(R.id.typePlanoValue).text.toString())
+            
             setResult(RESULT_OK, intent);
             finish()
+        }
+
+        spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(parent: AdapterView<*>, view: View, position: Int, id: Long) {
+                Toast.makeText(this@Activity_Funcionario_Planos_Treino_Add, options[position], Toast.LENGTH_LONG).show()
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>) {
+                // Do nothing
+            }
+        }
+
+        findViewById<Button>(R.id.CancelAddNewPlanButton).setOnClickListener{
+            startActivity(Intent(this@Activity_Funcionario_Planos_Treino_Add, Activity_Funcionario_Planos_Treino::class.java))
+        }
+
+        image_view.setOnClickListener {
+            spinner.performClick()
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if(requestCode == 100 && resultCode == RESULT_OK){
+            newImageValue = data?.data.toString()
+            findViewById<ImageView>(R.id.photoPlanoValue).setImageURI(data?.data)
         }
     }
 }
