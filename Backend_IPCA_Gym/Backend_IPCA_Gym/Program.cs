352be717d409a0deacee71ebd21385b2650c2255
Pedro Simões <69718437+L0ud3r@users.noreply.github.com>
2023-01-03 00:23:46 +0000

    Update Program.cs

--- Backend_IPCA_Gym/Backend_IPCA_Gym/Program.cs
+++ Backend_IPCA_Gym/Backend_IPCA_Gym/Program.cs
@@ -100,6 +100,8 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
+    app.UseDeveloperExceptionPage();
+
     app.UseSwagger();
     app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/Alpha/swagger.json", "IPCAGym Alpha"));
 }
