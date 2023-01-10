b8b6e028d8974cd0ddd685a2aa6ea3a69f6840b6
Goncalo Cunha <goncalo32661@gmail.com>
2023-01-10 21:11:29 +0000

    Pagina inicial cliente working
    Falta apresentar a lista de funcionarios usando o random das imagens.

--- IpcaGym2/app/src/main/java/vough/example/ipcagym/cliente_classes/Activity_Cliente_Pagina_Inicial.kt
+++ IpcaGym2/app/src/main/java/vough/example/ipcagym/cliente_classes/Activity_Cliente_Pagina_Inicial.kt
@@ -2,24 +2,53 @@ package vough.example.ipcagym.cliente_classes
 
 import android.content.Context
 import android.content.Intent
+import android.graphics.Color
+import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.AdapterView
-import android.widget.ArrayAdapter
-import android.widget.BaseAdapter
-import android.widget.ImageView
-import android.widget.Spinner
+import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
+import androidx.cardview.widget.CardView
+import androidx.lifecycle.lifecycleScope
 import vough.example.ipcagym.R
 import vough.example.ipcagym.data_classes.Plano_Treino
 import vough.example.ipcagym.data_classes.Refeicao
+import vough.example.ipcagym.requests.ClienteRequests
+import vough.example.ipcagym.requests.FuncionarioRequests
+import vough.example.ipcagym.requests.GinasioRequests
+import vough.example.ipcagym.requests.PedidoLojaRequests
 
 class Activity_Cliente_Pagina_Inicial : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_cliente_pagina_inicial)
+
+        //Buscar token
+        val preferences = getSharedPreferences("my_preferences", Context.MODE_PRIVATE)
+        val sessionToken = preferences.getString("session_token", null)
+
+        ClienteRequests.GetByToken(lifecycleScope, sessionToken) { resultCliente ->
+
+            //if (resultCliente?.foto_perfil != null)
+            //{
+            //    val imageUri: Uri = Uri.parse(resultCliente.foto_perfil)
+            //   imageView.setImageURI(imageUri)
+            //}
+
+            val name_view = findViewById<TextView>(R.id.textView7)
+            name_view.text = resultCliente?.nome
+
+            GinasioRequests.GetByID(lifecycleScope, sessionToken, resultCliente?.id_ginasio) { resultGym ->
+                if (resultGym != null) {
+                    if(resultGym.lotacao == resultGym.lotacaoMax){
+                        findViewById<TextView>(R.id.textView_capacidade_atual).setTextColor(Color.RED)
+                    }
+                    findViewById<TextView>(R.id.textView_capacidade_atual).text = "${resultGym.lotacao} / ${resultGym.lotacaoMax} "
+                }
+            }
+        }
 
         val imageView = findViewById<ImageView>(R.id.profile_pic_activity)
         val spinner = findViewById<Spinner>(R.id.spinner)
@@ -93,5 +122,32 @@ class Activity_Cliente_Pagina_Inicial : AppCompatActivity() {
         }
 
         imageView.setOnClickListener{ spinner.performClick() }
+        //TODO: Criar um layout para mostrar informação sobre nós
+        //findViewById<Button>(R.id.button_sobreNos).setOnClickListener {
+            //val intent = Intent(this@Activity_Cliente_Pagina_Inicial, ::class.java)
+            //startActivity(intent)
+
+        findViewById<CardView>(R.id.cardView_loja).setOnClickListener {
+            val intent = Intent(
+                this@Activity_Cliente_Pagina_Inicial,
+                Activity_Cliente_Loja_Produtos::class.java
+            )
+            startActivity(intent)
+        }
+
+        findViewById<CardView>(R.id.cardView_planos_nutricao).setOnClickListener{
+            val intent = Intent(this@Activity_Cliente_Pagina_Inicial,Activity_Cliente_Nutricao_Planos ::class.java)
+            startActivity(intent)
+        }
+
+        findViewById<Button>(R.id.button_view_team).setOnClickListener {
+            val intent = Intent(this@Activity_Cliente_Pagina_Inicial, Activity_Cliente_OurTeam::class.java)
+            startActivity(intent)
+        }
+
+        findViewById<Button>(R.id.button_avaliar).setOnClickListener{
+            val intent = Intent(this@Activity_Cliente_Pagina_Inicial,Activity_Cliente_Avaliar :: class.java)
+            startActivity(intent)
+        }
     }
 }
